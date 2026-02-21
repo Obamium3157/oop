b51becb0cc835940fbc6551e5c6f80296b7154c0
Obamium3157 <anatolijkovyralo@gmail.com>
2026-02-21 10:25:44 +0300

    Еще тесты для 2

--- lab1/task2/test/tests.sh
+++ lab1/task2/test/tests.sh
@@ -62,5 +62,42 @@ if "$PROGRAM" 10 2 > "$TMP_OUT" 2>&1; then fail; fi
 if ! grep -q "ERROR" "$TMP_OUT"; then fail; fi
 echo "PASSED"
 
-# TODO: выявить ограничения программы и засунуть в тесты
-# TODO: проверить граничные случаи
+echo "9: radix = 36"
+"$PROGRAM" 36 10 HELLO > "$TMP_OUT"
+if ! grep -q "29234652" "$TMP_OUT" 2>&1; then fail; fi
+echo "PASSED"
+
+echo "10: radix = 2"
+"$PROGRAM" 2 11 10011010110101 > "$TMP_OUT"
+if ! grep -q "7499" "$TMP_OUT" 2>&1; then fail; fi
+echo "PASSED"
+
+echo "11: MAX_INT"
+"$PROGRAM" 10 10 2147483647 > "$TMP_OUT"
+if ! grep -q "2147483647" "$TMP_OUT" 2>&1; then fail; fi
+echo "PASSED"
+
+echo "11: MIN_INT"
+"$PROGRAM" 10 10 "-2147483648" > "$TMP_OUT"
+if ! grep -q -- "-2147483648" "$TMP_OUT"; then fail; fi
+echo "PASSED"
+
+echo "11: MAX_INT + 1"
+if "$PROGRAM" 10 10 2147483648 > "$TMP_OUT" 2>&1; then fail; fi
+if ! grep -q "ERROR" "$TMP_OUT"; then fail; fi
+echo "PASSED"
+
+echo "12: MIN_INT - 1"
+if "$PROGRAM" 10 10 "-2147483649" > "$TMP_OUT" 2>&1; then fail; fi
+if ! grep -q "ERROR" "$TMP_OUT"; then fail; fi
+echo "PASSED"
+
+echo "11: MAX_INT to hex"
+"$PROGRAM" 10 16 2147483647 > "$TMP_OUT"
+if ! grep -q "7FFFFFFF" "$TMP_OUT" 2>&1; then fail; fi
+echo "PASSED"
+
+echo "12: MIN_INT to hex"
+"$PROGRAM" 10 16 "-2147483648" > "$TMP_OUT"
+if ! grep -q -- "-80000000" "$TMP_OUT" 2>&1; then fail; fi
+echo "PASSED"
