8e96d6ce679b00c345f235fb96c39e796ec6a022
Obamium3157 <anatolijkovyralo@gmail.com>
2026-02-28 10:43:31 +0300

    Форматирование 2-2

--- lab2/task2/html.cpp
+++ lab2/task2/html.cpp
@@ -4,7 +4,7 @@
 
 namespace
 {
-    const std::unordered_map<std::string, char> HTML_ENTITIES = {
+    const std::unordered_map<std::string, char> htmlEntities = {
         {"&quot;", '"'},
         {"&apos;", '\''},
         {"&lt;",   '<'},
@@ -12,9 +12,9 @@ namespace
         {"&amp;",  '&'},
     };
 
-    constexpr char ENTITY_START = '&';
-    constexpr char ENTITY_END = ';';
-    constexpr std::size_t MAX_ENTITY_LENGTH = 6;
+    constexpr char entityStart = '&';
+    constexpr char entityEnd = ';';
+    constexpr std::size_t maxEntityLength = 6;
 }
 
 std::string HtmlDecode(const std::string & html)
@@ -26,16 +26,16 @@ std::string HtmlDecode(const std::string & html)
 
     while (i < html.size())
     {
-        if (html[i] != ENTITY_START)
+        if (html[i] != entityStart)
         {
             result += html[i];
             ++i;
             continue;
         }
 
-        const std::size_t semicolonPos = html.find(ENTITY_END, i + 1);
+        const std::size_t semicolonPos = html.find(entityEnd, i + 1);
         const bool isTooFar = semicolonPos == std::string::npos
-            || semicolonPos - i + 1 > MAX_ENTITY_LENGTH;
+            || semicolonPos - i + 1 > maxEntityLength;
 
         if (isTooFar)
         {
@@ -45,9 +45,9 @@ std::string HtmlDecode(const std::string & html)
         }
 
         const std::string candidate = html.substr(i, semicolonPos - i + 1);
-        const auto it = HTML_ENTITIES.find(candidate);
+        const auto it = htmlEntities.find(candidate);
 
-        if (it == HTML_ENTITIES.end())
+        if (it == htmlEntities.end())
         {
             result += html[i];
             ++i;
